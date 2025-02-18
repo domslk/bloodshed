f8bbb3a0cb0aebed618dc91d5f734a8860ba703e
domenslak <domenslak08@gmail.com>
2025-02-18 20:22:16 +0100

    Update Clock.cs

--- scary game/Assets/Script/Clock.cs
+++ scary game/Assets/Script/Clock.cs
@@ -8,9 +8,28 @@ public class Clock : MonoBehaviour
     public int minutes = 0; //0 or 3
     [SerializeField] TextMeshProUGUI statusText;
 
+    private bool running = false;
 
+    void Start() {
+        StartCoroutine(clock());
+    }
 
-
+    void Update() {
+        if (!running) {
+            StartCoroutine(clock);
+        }
+    }
+    private IEnumerator clock() {
+        running = true;
+        yield return new WaitForSeconds(89);
+        time += 1;
+        if (minutes == 3) {
+            minutes = 0;
+        } else {
+            minutes = 3;
+        }
+        running = false;
+    }
     private void OnMouseEnter() {
         statusText.text = $"0{time}:{minutes}0 AM";
     }
