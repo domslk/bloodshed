0335f3a33f6075fb7537401861561852cd96277a
domenslak <domenslak08@gmail.com>
2025-02-22 14:52:58 +0100

    Update textShake.cs

--- Assets/Script/textShake.cs
+++ Assets/Script/textShake.cs
@@ -4,21 +4,24 @@ public class textShake : MonoBehaviour
 {   
 
     [SerializeField] TextMeshProUGUI text;
-    private TMP_TextInfo textInfo = text.textInfo;
-    private float characterCount = textInfo.characterCount;
+    private float textCharacterCount;
+    private TMP_TextInfo textInfo2;
     private int j = 0;
-    private Vector2 randomWiggle;
+    private float randomWiggle;
     private Vector3 currentPos;
     
     
     void Update()
     {
-        for (int i = 0; i < characterCount * 4; i++) { //cc*4 because 4 vert. for each renderred letter 
+        textInfo2 = text.textInfo;
+        textCharacterCount = textInfo2.characterCount;
+        for (int i = 0; i < textCharacterCount * 4; i++) { //cc*4 because 4 vert. for each renderred letter 
             if (j == 0) {
-                randomWiggle = (Random.Range(-0.03, 0.03), Random.Range(-0.03, 0.03))
+                randomWiggle = Random.Range(-0.03f, 0.03f);
             }
             currentPos = text.textInfo.meshInfo[0].vertices[i];
-            text.textInfo.meshInfo[0].vertices[i] = new Vector3();
+            text.textInfo.meshInfo[0].vertices[i] = new Vector3(currentPos.x + randomWiggle, currentPos.y + randomWiggle, currentPos.z);
+            j += 1;
         }
     }
 }
