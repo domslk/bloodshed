17f818d11a63ad262adbb4f26696ff349f9cc820
domenslak <domenslak08@gmail.com>
2025-02-22 15:29:25 +0100

    Update textShake.cs

--- Assets/Script/textShake.cs
+++ Assets/Script/textShake.cs
@@ -14,22 +14,21 @@ public class textShake : MonoBehaviour
     
     void Update()
     {   
-        j = 0;
-        textInfo2 = text.textInfo;
+        textInfo2 = text.textInfo; 
         textCharacterCount = textInfo2.characterCount;
-        for (int i = 0; i < textCharacterCount * 4; i++) { //cc*4 because 4 vert. for each renderred letter 
-            if (j == 0) {
-                randomWiggleX = Random.Range(-0.5f, 0.5f);
-                randomWiggleY = Random.Range(-0.5f, 0.5f);
-            } else if (j == 4) {
-                j = 0;
+        for (int i = 0; i < textCharacterCount; i++) { 
+            int vertexIndex = text.textInfo.characterInfo[i].vertexIndex;
+            randomWiggleX = Random.Range(-0.1f, 0.1f);
+            randomWiggleY = Random.Range(-0.1f, 0.1f);
+            for (int j = 0; j < 4; j++) {
+                currentPos = text.textInfo.meshInfo[0].vertices[vertexIndex + j]; 
+                text.textInfo.meshInfo[0].vertices[vertexIndex + j] = new Vector3(currentPos.x + randomWiggleX, currentPos.y + randomWiggleY, currentPos.z);
             }
-            currentPos = text.textInfo.meshInfo[0].vertices[i];
-            text.textInfo.meshInfo[0].vertices[i] = new Vector3(currentPos.x + randomWiggleX, currentPos.y + randomWiggleY, currentPos.z);
-            j += 1;
+            //apply new vertices
+            text.textInfo.meshInfo[0].mesh.vertices = text.textInfo.meshInfo[0].vertices;
+            
         }
-        //apply new vertices
-        text.textInfo.meshInfo[0].mesh.vertices = text.textInfo.meshInfo[0].vertices;
         text.UpdateVertexData();
+        
     }
 }
