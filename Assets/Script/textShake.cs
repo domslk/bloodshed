75acac1b321a05731ed78cedac52adf72c60a8fc
domenslak <domenslak08@gmail.com>
2025-02-22 15:03:52 +0100

    textShake works now
    holy moly

--- Assets/Script/textShake.cs
+++ Assets/Script/textShake.cs
@@ -7,21 +7,29 @@ public class textShake : MonoBehaviour
     private float textCharacterCount;
     private TMP_TextInfo textInfo2;
     private int j = 0;
-    private float randomWiggle;
+    private float randomWiggleX;
+    private float randomWiggleY;
     private Vector3 currentPos;
     
     
     void Update()
-    {
+    {   
+        j = 0;
         textInfo2 = text.textInfo;
         textCharacterCount = textInfo2.characterCount;
         for (int i = 0; i < textCharacterCount * 4; i++) { //cc*4 because 4 vert. for each renderred letter 
             if (j == 0) {
-                randomWiggle = Random.Range(-0.03f, 0.03f);
+                randomWiggleX = Random.Range(-0.5f, 0.5f);
+                randomWiggleY = Random.Range(-0.5f, 0.5f);
+            } else if (j == 4) {
+                j = 0;
             }
             currentPos = text.textInfo.meshInfo[0].vertices[i];
-            text.textInfo.meshInfo[0].vertices[i] = new Vector3(currentPos.x + randomWiggle, currentPos.y + randomWiggle, currentPos.z);
+            text.textInfo.meshInfo[0].vertices[i] = new Vector3(currentPos.x + randomWiggleX, currentPos.y + randomWiggleY, currentPos.z);
             j += 1;
         }
+        //apply new vertices
+        text.textInfo.meshInfo[0].mesh.vertices = text.textInfo.meshInfo[0].vertices;
+        text.UpdateVertexData();
     }
 }
