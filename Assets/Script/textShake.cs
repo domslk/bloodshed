152dd68d6dcefcab9c923ae8603a8798722b7c71
domenslak <domenslak08@gmail.com>
2025-02-22 15:50:45 +0100

    Update textShake

--- Assets/Script/textShake.cs
+++ Assets/Script/textShake.cs
@@ -2,33 +2,16 @@ using UnityEngine;
 using TMPro;
 public class textShake : MonoBehaviour
 {   
-
     [SerializeField] TextMeshProUGUI text;
-    private float textCharacterCount;
-    private TMP_TextInfo textInfo2;
-    private int j = 0;
-    private float randomWiggleX;
-    private float randomWiggleY;
-    private Vector3 currentPos;
-    
-    
+    private float wiggleAmount;
+    private Vector3 position = new Vector3(-123f, -282f, 0f);
+
+    public bool animationEnd = false;
     void Update()
     {   
-        textInfo2 = text.textInfo; 
-        textCharacterCount = textInfo2.characterCount;
-        for (int i = 0; i < textCharacterCount; i++) { 
-            int vertexIndex = text.textInfo.characterInfo[i].vertexIndex;
-            randomWiggleX = Random.Range(-0.1f, 0.1f);
-            randomWiggleY = Random.Range(-0.1f, 0.1f);
-            for (int j = 0; j < 4; j++) {
-                currentPos = text.textInfo.meshInfo[0].vertices[vertexIndex + j]; 
-                text.textInfo.meshInfo[0].vertices[vertexIndex + j] = new Vector3(currentPos.x + randomWiggleX, currentPos.y + randomWiggleY, currentPos.z);
-            }
-            //apply new vertices
-            text.textInfo.meshInfo[0].mesh.vertices = text.textInfo.meshInfo[0].vertices;
-            
-        }
-        text.UpdateVertexData();
-        
+        wiggleAmount = Random.Range(-0.2f, 0.2f);
+        text.transform.position = new Vector3(text.transform.position.x + wiggleAmount, text.transform.position.y + wiggleAmount, text.transform.position.z);
     }
+
+
 }
