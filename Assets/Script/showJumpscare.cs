6572b7cad9ee233cbb772919d3ee1fa48a481fbf
domenslak <domenslak08@gmail.com>
2025-02-23 17:54:55 +0100

    New sounds and background

--- Assets/Script/showJumpscare.cs
+++ Assets/Script/showJumpscare.cs
@@ -8,16 +8,24 @@ public class showJumpscare : MonoBehaviour
     public VideoPlayer videoPlayer;
     public GameObject canvasVideo;
     public GameObject canvas;
+    public GameObject canvasVideoImg;
+    public GameObject canvasVideoVideo;
     public GameObject text;
 
 
     void Start() {
+        canvas.SetActive(true);
+        StartCoroutine(StartVideo());
+    }
+
+    private IEnumerator StartVideo() {
         videoPlayer.Play();
-        canvasVideo.SetActive(true);
+        canvasVideoVideo.SetActive(true);
+        yield return new WaitForSeconds(0.25f);
+        canvasVideoImg.SetActive(false);
         canvas.SetActive(false);
         StartCoroutine(noVideo(1));
     }
-
     private IEnumerator noVideo(int wait) {
         yield return new WaitForSeconds(wait);
         canvas.SetActive(true);
